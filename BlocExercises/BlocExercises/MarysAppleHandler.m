18db9086bde18481997da9f1a245f7100ed0cd21
Pankaj Vohra <pjvohra@gmail.com>
2015-12-06 19:53:36 -0500

    11.if/Else Completed

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -15,20 +15,28 @@
 
     /* WORK HERE */
     
+    if (dollars >= 100000000) {
+        itemToReturn = @"have The Big Apple";
+    } else if (dollars >= 1000) {
+        itemToReturn = @"have an Apple computer";
+    } else if (dollars >= 6) {
+        itemToReturn = @"have an apple";
+    } else if (dollars >= 5) {
+        itemToReturn = @"have some gum";
+    } else {
+        itemToReturn = @"get out of my store";
+    }
+   
+    
     NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
     return itemToReturn;
 }
 
 - (NSUInteger) dollarCostForAppleFlavoredVodka {
     /* WORK HERE */
+    return self.getsDiscount ? 18:24;
+    
 
-    NSUInteger cost = 24;
-    
-    if (self.getsDiscount) {
-        cost *= .75;
-    }
-    
-    return cost;
 }
 
-@end
+    @end
